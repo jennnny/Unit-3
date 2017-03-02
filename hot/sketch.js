2dbd6b919c20dca26ff22b1a1c73b7a5f3b6d910
jennnny <lemon.ti@hotmail.com>
2017-03-02 23:48:54 +0000

    Add files via upload

---
+++ hot/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+function draw() {
+  background(248);
+  textFont("Seravek");
+  textStyle(BOLD);
+  textAlign(CENTER);
+  
+  fill(245,235,0)
+  textSize(42);
+  text("hot",windowWidth / 2 , windowHeight / 2.05+sin(frameCount*0.2));
+  textSize(40);
+  fill(235,55,0)  
+  text("hot",windowWidth / 2 , windowHeight / 2);
+  
+} 
